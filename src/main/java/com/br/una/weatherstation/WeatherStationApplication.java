b1125feb1dfa32eee49f88ba8864ac0e7797b889
Almadavic <almadavic@live.com>
2022-11-27 11:47:35 -0300

    V4.2.1

--- src/main/java/com/br/una/weatherstation/WeatherStationApplication.java
+++ src/main/java/com/br/una/weatherstation/WeatherStationApplication.java
@@ -2,8 +2,10 @@ package com.br.una.weatherstation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class WeatherStationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/br/una/weatherstation/config/ExtraConfig.java
+++ src/main/java/com/br/una/weatherstation/config/ExtraConfig.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-@Configuration
+//@Configuration
 public class ExtraConfig implements CommandLineRunner {
 
     @Autowired
